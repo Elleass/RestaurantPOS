73e25128c03b3ee1021bcad8b87cf908bf732dc9
Elleass <mic.kalinowski@protonmail.com>
2025-05-14 11:38:50 +0200

    Basic project setup

---
+++ src/test/java/org/example/restaurantpos/RestaurantPosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restaurantpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
