312a832a4b518e37807f7def7341cb9d475d8457
Elleass <mic.kalinowski@protonmail.com>
2025-05-30 16:30:24 +0200

    Add flywheel migration scripts

---
+++ src/main/resources/db/migration/V2__create_roles.sql
@@ -0,0 +1,5 @@
+-- V2__create_roles.sql
+INSERT INTO roles (role_name, description) VALUES
+    ('ROLE_ADMIN', 'Administrator'),
+    ('ROLE_USER', 'Regular user');
+
---
+++ src/main/resources/db/migration/V3__create_admin_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password, is_locked, id_role) VALUES
+    ('admin', '$2a$10$DOWSDMDPNu2sZbOQecIH/O5JgxAOJc9gETyp1VOZNYEijyykffwmy', false, 1);
