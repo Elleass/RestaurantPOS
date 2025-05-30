703bab2ed284857d5cf0b4f3001be6c695db9038
Elleass <mic.kalinowski@protonmail.com>
2025-05-30 16:28:03 +0200

    Delete RoleRepository

--- src/main/java/org/example/restaurantpos/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.restaurantpos.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.example.restaurantpos.entity.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {}
